ba889dcc4fa241f31463a4bad2b8fd3a52772890
Vardan <vardan.vardanyan.fd@gmail.com>
2021-08-19 22:29:37 +0400

    add run function in data_filter module

--- data_filter.py
+++ data_filter.py
@@ -38,3 +38,51 @@ def sort(table: list, n: int, sort_arg: str) -> list:
     right = sort(right, len(right), sort_arg)
     ans = left+middle+right
     return ans
+
+
+def run() -> None:
+    """
+    run method starts filtering process.
+    All filtering functions are called here,
+    and almost all important objects are created
+    here in this file
+    """
+
+    global star_cnt
+    global characteristics
+    print("Data Filter process is started")
+    inp = in_out.get_in()
+    start_time = time.time()
+    table = []
+    res = in_out.read_store(table, config['DEFAULT']['file_name'], inp.ra, inp.dec, inp.fov_h, inp.fov_v)
+    table = res[0]
+    star_cnt = res[1]
+    """sort stars by magnitude"""
+    table = sort(table, star_cnt, 'phot_g_mean_mag')
+
+    '''
+    This statement checks if we have more than N stars left,
+    if we do, we need to take the brightest N ones
+    '''
+    if inp.n < star_cnt:
+        star_cnt = inp.n
+        table = table[:star_cnt]
+
+    '''
+    This part of code adds new characteristic to our table:
+    'dist' which shows the distance between given point in input
+    and the particular star
+    '''
+    characteristics.append('dist')
+    for i in range(star_cnt):
+        star1 = star.Star(inp.ra, inp.dec)
+        star2 = star.Star(table[i].ra, table[i].dec)
+        dist = star.ang_dist(star1, star2)
+        table[i].dist = dist
+
+    table = sort(table, star_cnt, 'dist')
+
+    in_out.write(table, star_cnt, str(str(datetime.now())+'.csv'))
+
+    end_time = time.time()
+    print('Execution time: ', end_time-start_time, 'Sec')
